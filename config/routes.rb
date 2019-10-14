d37ae68d690149c9d5686d3d6c8822ff5380385a
João Almeida <joaorsalmeida@gmail.com>
2019-10-14 15:22:08 -0300

    Add employee and candidate devise models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :employees
+  devise_for :candidates
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
