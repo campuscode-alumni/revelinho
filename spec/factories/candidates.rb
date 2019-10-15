eee1ccfdafe2d7454529fe0b8ae42a1c43eb70b9
Gustavo Iha <gustavo.iha@revelo.com.br>
2019-10-15 11:24:15 -0300

    Added city, state, country and zip code

--- spec/factories/candidates.rb
+++ spec/factories/candidates.rb
@@ -7,6 +7,10 @@ FactoryBot.define do
     address { 'Rua Revelada, 10' }
     occupation { 'full stack developer' }
     educational_level { 'post-doc' }
+    city { 'São Paulo' }
+    state { 'São Paulo' }
+    country { 'Brasil' }
+    zip_code { '03267-080' }
     status { :published }
 
     trait :hidden do
