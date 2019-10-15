54dea64e032a7f172f2ab78de5ed148c1b161a5d
Gustavo Iha <gustavo.iha@revelo.com.br>
2019-10-15 09:55:27 -0300

    Generating candiadates in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Candidate.create!(
+  name: 'Lucas',
+  email: 'lucas@candidato.com',
+  password: '123456',
+  cpf: '1234567890',
+  address: 'Rua Revelada, 10',
+  occupation: 'full stack developer',
+  educational_level: 'mestrado concluído',
+  status: :published
+)
+Candidate.create!(
+  name: 'Rafael',
+  email: 'rafael@candidato.com',
+  password: '123456',
+  cpf: '1234567890',
+  address: 'Rua Revelada, 10',
+  occupation: 'back-end node.js developer',
+  educational_level: 'graduação em andamento',
+  status: :published
+)
+Candidate.create!(
+  name: 'Gustavo',
+  email: 'gustavo@candidato.com',
+  password: '123456',
+  cpf: '1234567890',
+  address: 'Rua Revelada, 10',
+  occupation: 'web developer',
+  educational_level: 'graduação completa',
+  status: :published
+)
+Candidate.create!(
+  name: 'Patrícia',
+  email: 'patricia@candidato.com',
+  password: '123456',
+  cpf: '1234567890',
+  address: 'Rua Revelada, 10',
+  occupation: 'dev ops guru',
+  educational_level: 'mestrado concluído',
+  status: :published
+)
