bb542b91fb0fd90f8f5f788a342c06e09f10992e
Stefano Butori <butori@gmail.com>
2019-10-15 17:01:15 -0300

    Restrict routes for positions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :candidates
   resources :candidates, only: %i[index show]
   root 'home#index'
-  resources :positions
+  resources :positions, only: %i[new create show]
 end
