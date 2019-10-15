28a12ae90a0df09bbb809d0aeda22d4b77a2c254
LucasKendi <hiranolucas@gmail.com>
2019-10-15 13:05:58 -0300

    add company creation to employee devise controller

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,22 +2,6 @@ class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
-  after_create :company_exists
-
-  belongs_to :company, required: false
-
-  private
-
-  def get_company(name, domain)
-    Company.find_by(url_domain: domain) || Company.create(name: name, url_domain: domain)
-  end
-
-  def company_exists
-    company_domain = email.split('@')[1]
-    company_name = company_domain.split('.')[0].humanize
-    company = get_company(company_name, company_domain)
-    
-    self.update_attributes(company: company)
-  end
+         :recoverable, :rememberable, :validatable
+  belongs_to :company, optional: true
 end
