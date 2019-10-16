d2ad6c51bdca04fd3423e3bff514b2f2a69c4f09
Derick <derickedu@gmail.com>
2019-10-16 10:26:22 -0300

    candidate complete profile successfully test

--- spec/features/candidate/candidate_complete_profile_spec.rb
+++ spec/features/candidate/candidate_complete_profile_spec.rb
@@ -10,14 +10,22 @@ feature 'Candidate completes personal profile' do
 
     fill_in 'Experiência profissional', with: 'Revelo'
     fill_in 'Formação', with: 'Faculdade X'
-    fill_in 'Habilidades', with: 'Ruby on Rails'
-    fill_in 'Línguas', with: 'Inglês'
+    fill_in 'Linguagens de Programação', with: 'Ruby'
+    fill_in 'Habilidades', with: 'Scrum'
+    fill_in 'Usuário do Skype', with: 'candidate.skype'
+    fill_in 'Linkedin', with: 'https://www.linkedin.com/in/candidate'
+    fill_in 'Git', with: 'https://github.com/candidate'
+    fill_in 'Proficiência em Inglês', with: 'Fluente'
     click_on 'Atualizar'
 
     expect(page).to have_content('Revelo')
     expect(page).to have_content('Faculdade X')
-    expect(page).to have_content('Ruby on Rails')
-    expect(page).to have_content('Inglês')
+    expect(page).to have_content('Ruby')
+    expect(page).to have_content('Scrum')
+    expect(page).to have_content('candidate.skype')
+    expect(page).to have_content('https://www.linkedin.com/in/candidate')
+    expect(page).to have_content('https://github.com/candidate')
+    expect(page).to have_content('Fluente')
     candidate.reload
     expect(candidate).to be_published
   end
