36265da8a5379418a382b54c7e9f9397c85ca3ff
Stefano Butori <butori@gmail.com>
2019-10-18 15:47:47 -0300

    Remove comments on HomeController#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def index
     redirect_to dashboard_candidates_path if candidate_signed_in?
-    # elsif employee_signed_in?
-    # redirect_to dashboard_employees_path
-    # end
   end
 end
