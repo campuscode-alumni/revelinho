d2e2991db7a978b0bd930f5bd802342d71d31346
Pekorishia <pcruz.pat@gmail.com>
2019-10-18 09:47:16 -0300

    included invite relation with candidate and position

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,2 +1,6 @@
 class Invite < ApplicationRecord
+  belongs_to :candidate, dependent: :destroy
+  belongs_to :position, dependent: :destroy
+
+  enum status: { pending: 0, rejected: 5, accepted: 10 }
 end
