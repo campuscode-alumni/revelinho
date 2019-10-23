be48f91e42fe27cf3bd1e7832938c72212871f92
rafaeldtx <rafael991_@hotmail.com>
2019-10-23 17:33:06 -0300

    WIP unity test for decorators

--- spec/decorators/selection_process_decorator_spec.rb
+++ spec/decorators/selection_process_decorator_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SelectionProcessDecorator do
+  context '#contract_type' do
+    it 'show range of ' do
+      invite = create(:invite).decorate
+      expect(invite.invite_links).to eq ''
+    end
+  end
 end
