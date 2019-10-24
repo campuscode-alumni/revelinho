28d54c5d59f9c0d4b9bf1ae9fed4d3ecf8f76525
LucasKendi <hiranolucas@gmail.com>
2019-10-24 14:08:08 -0300

    add interview invite card

---
+++ spec/features/interview/candidate_accept_interview_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'candidate accept interview' do
+  scenario 'successfully' do
+    company = create(:company, name: 'Revelo', url_domain: 'revelo.com.br')
+    candidate = create(:candidate, status: :published)
+    create(:employee, email: 'joao@revelo.com.br', company: company)
+    position = create(:position, company: company)
+    invite = create(:invite, candidate: candidate, position: position,
+                             status: :pending)
+    selection_process = invite.create_selection_process
+    create(:interview, datetime: '2019-10-26 17:00:00',
+                       format: :face_to_face,
+                       address: 'Av. Paulista, 2000',
+                       selection_process: selection_process)
+
+    login_as(candidate, scope: :candidate)
+    visit selection_process_candidates_path(selection_process)
+
+    expect(page).to have_content('Você tem um novo convite para entrevista')
+    expect(page).to have_content('Dia 26/10/2019 às 17:00')
+    expect(page).to have_content('Local: Av. Paulista, 2000')
+    expect(page).to have_content('Formato: presencial')
+    expect(page).to have_button('Aceitar')
+    expect(page).to have_button('Rejeitar')
+  end
+end
