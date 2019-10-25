901e19150e729fd2d34aedbe51a6eab0346c69b0
LucasKendi <hiranolucas@gmail.com>
2019-10-25 14:32:41 -0300

    Add status on interviews tests and view

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,12 +1,15 @@
 class InterviewsController < ApplicationController
   before_action :set_interview, only: %i[accept reject]
+  before_action :authenticate_candidate!, only: %i[accept reject]
 
   def accept
     @interview.scheduled!
+    redirect_to selection_process_candidates_path(@interview.selection_process)
   end
 
   def reject
     @interview.canceled!
+    redirect_to selection_process_candidates_path(@interview.selection_process)
   end
 
   private
--- app/decorators/interview_decorator.rb
+++ app/decorators/interview_decorator.rb
@@ -7,4 +7,32 @@ class InterviewDecorator < Draper::Decorator
     'Dia ' + interview.datetime.strftime('%d/%m/%Y') +
       ' às ' + interview.datetime.strftime('%R')
   end
+
+  def decision_buttons
+    return '' unless interview.pending?
+
+    reject_button + accept_button
+  end
+
+  def interview_status_badge
+    if interview.pending?
+      content_tag(:span, 'Aguardando resposta', class: 'badge badge-warning')
+    elsif interview.scheduled?
+      content_tag(:span, 'Entrevista agendada', class: 'badge badge-primary')
+    elsif interview.canceled?
+      content_tag(:span, 'Entrevista cancelada', class: 'badge badge-danger')
+    end
+  end
+
+  private
+
+  def accept_button
+    link_to 'Aceitar', accept_interview_candidate_path(interview),
+            class: 'btn btn-outline-success', method: :post
+  end
+
+  def reject_button
+    link_to 'Recusar', reject_interview_candidate_path(interview),
+            class: 'btn btn-outline-danger', method: :post
+  end
 end
