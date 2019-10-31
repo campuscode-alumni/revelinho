3daf6dabe7ebcf9e9279e5f3553c03b596cbae4c
Gustavo Iha <gustavo.iha@revelo.com.br>
2019-10-31 10:54:44 -0300

    added decorator tests

--- spec/decorators/company_profile_decorator_spec.rb
+++ spec/decorators/company_profile_decorator_spec.rb
@@ -19,4 +19,43 @@ describe CompanyProfileDecorator do
       expect(company_link).to include('Completar perfil da empresa')
     end
   end
+
+  context '#card_render' do
+    it 'shows pending invites count' do
+      company = create(:company, url_domain: 'revelo.com.br')
+      candidate1 = create(:candidate)
+      candidate2 = create(:candidate)
+      candidate3 = create(:candidate)
+      create(:candidate_profile, candidate: candidate1)
+      create(:candidate_profile, candidate: candidate2)
+      create(:candidate_profile, candidate: candidate3)
+      position = create(:position, title: 'Engenheiro de Software Pleno',
+                                   company: company)
+      create(:invite, position: position,
+                      candidate: candidate1)
+      create(:invite, position: position,
+                      candidate: candidate2)
+      create(:invite, :accepted, position: position,
+                                 candidate: candidate3)
+      card_render = CompanyProfileDecorator.decorate(company).card_render
+
+      expect(card_render).to include('2')
+      expect(card_render).to include('Convites')
+    end
+
+    it 'shows pending interviews count' do
+      company = create(:company, url_domain: 'revelo.com.br')
+      candidate = create(:candidate)
+      create(:candidate_profile, candidate: candidate)
+      position = create(:position, company: company)
+      invite = create(:invite, position: position, candidate: candidate)
+      selection_process = create(:selection_process, invite: invite)
+      create(:interview, selection_process: selection_process)
+      create(:interview, selection_process: selection_process)
+      card_render = CompanyProfileDecorator.decorate(company).card_render
+
+      expect(card_render).to include('2')
+      expect(card_render).to include('Entrevistas')
+    end
+  end
 end
