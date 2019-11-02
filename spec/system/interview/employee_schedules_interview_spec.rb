93b6c4e34e8d9a5f8fc68b04de18dd2de6f15492
Derick <derickedu@gmail.com>
2019-11-02 16:09:10 -0300

    remove js test

--- spec/system/interview/employee_schedules_interview_spec.rb
+++ spec/system/interview/employee_schedules_interview_spec.rb
@@ -1,50 +1,6 @@
 require 'rails_helper'
 
 feature 'Employee schedules interview' do
-  scenario 'successfully', js: true do
-    company = create(:company, url_domain: 'revelo.com.br')
-    create(:company_profile, company: company)
-    employee = create(:employee, email: 'renata@revelo.com.br',
-                                 company: company)
-    candidate = create(:candidate, name: 'Gustavo')
-    create(:candidate_profile, candidate: candidate)
-    position = create(:position, title: 'Engenheiro de Software Pleno',
-                                 company: company)
-    invite = create(:invite, position: position, candidate: candidate,
-                             status: :accepted)
-    selection_process = create(:selection_process, invite: invite)
-    interview = build(:interview, date: DateTime.parse('25/11/2019'))
-
-    login_as(employee, scope: :employee)
-
-    visit new_selection_process_interview_path(selection_process)
-    click_on 'interview-modal-button'
-
-    find('#date-field > div > i > svg').click
-    if has_css?('.ant-calendar-selected-day')
-      select_day_input = find('.ant-calendar-selected-day')
-      select_day_input.click
-    else
-      find('#date-field > div > i > svg').click
-      find('.ant-calendar-selected-day').click
-    end
-
-    find('#time-from-field > input').click
-    find('.ant-time-picker-panel-select-option-selected', match: :first).click
-
-    find('#time-to-field > input').click
-    find('li', text: '8').click
-
-    fill_in 'address-field', with: interview.address
-
-    find('label', text: 'Online').click
-
-    click_on 'OK'
-
-    expect(page).to have_content('Entrevista salva')
-    expect(Interview.count).to eq(1)
-  end
-
   scenario 'validates empty field', :js do
     company = create(:company, url_domain: 'revelo.com.br')
     create(:company_profile, company: company)
