d5b7aac004e743c6eeecb11f1fe3b891dac9361e
Gustavo Iha <gustavo.iha@revelo.com.br>
2019-11-03 09:40:49 -0300

    Set capybara max wait time back to default (2)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,8 +60,6 @@ RSpec.configure do |config|
 
   Capybara.server = :puma, { Silent: true }
 
-  Capybara.default_max_wait_time = 3
-
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
